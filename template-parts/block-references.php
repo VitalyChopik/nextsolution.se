61a2138df5efd82f0cf3b2ce29fdb50cc9620de2
VitalyChopik <leo.space97@gmail.com>
2023-03-31 17:57:17 +0300

    сделал бекенд для блока

--- template-parts/block-references.php
+++ template-parts/block-references.php
@@ -15,17 +15,12 @@ $page_fields = get_fields();
 $additional_class     = (array_key_exists('className', $block))
     ? $block['className']
     : '';
-$block_title          = getFieldValue($page_fields, 'block_title');
-$block_text           = getFieldValue($page_fields, 'block_text');
-$title_tag = getFieldValue($page_fields, 'title_tag');
-
+$image_url= getFieldValue($page_fields, 'image_url');
+$name = getFieldValue($page_fields, 'name');
+$position = getFieldValue($page_fields, 'position');
+$linksTitle = getFieldValue($page_fields, 'links-title');
+$links= getFieldValue($page_fields, 'links');
 ?>
-
-<?php echo ($title_tag) ? do_shortcode($title_tag) : ''; ?>
-<?php echo ($cta_title)
-			? do_shortcode($cta_title)
-			: ''; ?>
-
 
 <div class="references <?php echo $additional_class; if($page_post === false){ ?> ctaction__page<?php }?>" <?php if(!empty(get_field('block_id'))){ echo 'id="'. get_field('block_id') .'"'; } ?>>
 	<div class="references__block">
@@ -37,26 +32,36 @@ $title_tag = getFieldValue($page_fields, 'title_tag');
               'w' => 70,
               'h' => 70,
           ));
-        ?>
+          ?>
           <svg viewBox="0 0 420 420" role="img" class="image-mask" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
               <?php $rand_id = bin2hex(random_bytes(10)); ?>
               <mask id="svgmask-<?php echo $rand_id; ?>">
                   <path fill="#ffffff" d="M58.1,84.3C137.8,15,316.4-40.2,369.2,38.7c52.8,78.9,66.3,178.4,31.7,250.2c-20,41.5-44.1,58-81.9,81.7
-c-87.8,55.2-187.9,75.8-260.9,0C-17,292.7-21.7,153.6,58.1,84.3z" />
+    c-87.8,55.2-187.9,75.8-260.9,0C-17,292.7-21.7,153.6,58.1,84.3z" />
               </mask>
               <image width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo $image_url ?>" mask="url(#svgmask-<?php echo $rand_id; ?>)"></image>
           </svg>
         </div>
        <div class="references__author-text">
-        <span class="references__author-name">Teo Fagerström</span>
-        <span class="references__author-position">Digital Specialist</span>
+        <span class="references__author-name"><?php echo ($name) ? do_shortcode($name) : ''; ?></span>
+        <span class="references__author-position"><?php echo ($position) ? do_shortcode($position) : ''; ?></span>
        </div>
     </div>
     <div class="references__links">
-      <span class="references__links-title">References:</span>
-      <a href="#" class="references__link">Ingen SEO-strategi utan en SEO-analys</a>
-      <a href="#" class="references__link">När behövs en SEO-analys?</a>
-      <a href="#" class="references__link">Hur du utför en fullständig SEO-webbplatsanalys</a>
+      <span class="references__links-title"><?php echo ($linksTitle) ? do_shortcode($linksTitle) : ''; ?></span>
+      <?php if ($links) : ?>
+        <?php foreach ($links as $link) : ?>
+          <?php
+          $link__text = (array_key_exists('text', $link))
+          ? $link['text']
+          : '';
+          $link_item = (array_key_exists('link_item', $link))
+          ? $link['link_item']
+          : '';
+          ?>
+          <a href="<?php echo $link_item; ?>" class="references__link"><?php echo $link__text; ?></a>
+        <?php endforeach; ?>
+      <?php endif; ?>
     </div>
 	</div>
 </div>
